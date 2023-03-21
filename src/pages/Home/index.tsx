eb1f4451d0f4a3e6d1473e45169d63491a989a79
emanoelantonio <dev.emanoel@protonmail.com>
2023-03-21 17:17:58 -0300

    feat: Validations

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,51 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import * as zod from 'zod';
+
 import { Play } from 'phosphor-react';
-import { CountDownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartButton, TaskInput } from './styles';
+import * as S from './styles';
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod.number().min(5).max(60),
+});
+
+interface NewCycleFormData {
+  task: string;
+  minutesAmount: number;
+}
 
 export const Home = () => {
+
+  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  });
+
+  function handleCreateNewCycle(data:NewCycleFormData) {
+    console.log(data);
+  }
+
+  const task = watch('task');
+  const isSubmitDisabled = !task;
+
   return (
-    <HomeContainer>
-      <form action="">
-        <FormContainer>
+    <S.HomeContainer>
+
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
+
+        <S.FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput type="text" id="task" list='task-suggestions' placeholder='Dê um nome para o seu projeto'/>
+          <S.TaskInput
+            type="text"
+            id="task"
+            list='task-suggestions'
+            placeholder='Dê um nome para o seu projeto'
+            {...register('task')}
+          />
 
           <datalist id='task-suggestions'>
             <option value="Aula de JS"/>
@@ -16,31 +54,32 @@ export const Home = () => {
           </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
-          <MinutesAmountInput
+          <S.MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder='00'
             step={5}
             min={5}
             max={60}
+            {...register('minutesAmount', {valueAsNumber: true})}
           />
 
           <span>minutos.</span>
-        </FormContainer>
+        </S.FormContainer>
 
-        <CountDownContainer>
+        <S.CountDownContainer>
           <span>0</span>
           <span>0</span>
-          <Separator>:</Separator>
+          <S.Separator>:</S.Separator>
           <span>0</span>
           <span>0</span>
-        </CountDownContainer>
+        </S.CountDownContainer>
 
-        <StartButton type="submit">
+        <S.StartButton disabled={isSubmitDisabled} type="submit">
           <Play size={24}/>
           Iniciar
-        </StartButton>
+        </S.StartButton>
       </form>
-    </HomeContainer>
+    </S.HomeContainer>
   );
 };
